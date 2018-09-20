df08a6c6fab41017d404d5d8b37f7ad2f67c4484
杨钊 <yangzhao_java@163.com>
2018-09-21 01:05:56 +0800

    时间工具类 新增两个方法： 1.获取某年某月最后一天 2.获取某年某月第一天

--- common-core/src/main/java/com/yz/common/core/utils/DateUtils.java
+++ common-core/src/main/java/com/yz/common/core/utils/DateUtils.java
@@ -692,4 +692,49 @@ public class DateUtils {
 		calendar.add(Calendar.MONTH, month);
 		return calendar.getTime();
 	}
+
+	/**
+	 * 获取某年某月第一天
+	 * @param year
+	 * @param month
+	 * @return
+	 */
+	public static String getFirstDayOfMonth(int year,int month){
+		Calendar cal = Calendar.getInstance();
+		//设置年份
+		cal.set(Calendar.YEAR,year);
+		//设置月份
+		cal.set(Calendar.MONTH, month-1);
+		//获取某月最小天数
+		int firstDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH);
+		//设置日历中月份的最小天数
+		cal.set(Calendar.DAY_OF_MONTH, firstDay);
+		//格式化日期
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String firstDayOfMonth = sdf.format(cal.getTime());
+		return firstDayOfMonth;
+	}
+
+	/**
+	 * 获取某年某月最后一天
+	 * @param year
+	 * @param month
+	 * @return
+	 */
+	public static String getLastDayOfMonth(int year,int month){
+		Calendar cal = Calendar.getInstance();
+		//设置年份
+		cal.set(Calendar.YEAR,year);
+		//设置月份
+		cal.set(Calendar.MONTH, month-1);
+		//获取某月最大天数
+		int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		//设置日历中月份的最大天数
+		cal.set(Calendar.DAY_OF_MONTH, lastDay);
+		//格式化日期
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String lastDayOfMonth = sdf.format(cal.getTime());
+		return lastDayOfMonth;
+	}
+
 }
