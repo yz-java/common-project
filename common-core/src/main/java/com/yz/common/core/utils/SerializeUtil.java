f9d9b4d9c68717b00852814c67ecd1ecd4cf83ad
yangzhao <yangzhao_java@163.com>
2018-11-26 10:34:33 +0800

    no message

--- common-core/src/main/java/com/yz/common/core/utils/SerializeUtil.java
+++ common-core/src/main/java/com/yz/common/core/utils/SerializeUtil.java
@@ -1,12 +1,10 @@
 package com.yz.common.core.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 /**
  * 对象序列化、反序列化工具类
+ *
  * @Auther: yangzhao
  * @Date: 2018/11/26 10:28
  * @Description:
@@ -14,40 +12,32 @@ import java.io.ObjectOutputStream;
 public class SerializeUtil {
     /**
      * 序列化
+     *
      * @param object
      * @return
      */
-    public static byte[] serialize(Object object) {
+    public static byte[] serialize(Object object) throws IOException {
         ObjectOutputStream oos = null;
         ByteArrayOutputStream baos = null;
-        try {
-            // 序列化
-            baos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(baos);
-            oos.writeObject(object);
-            byte[] bytes = baos.toByteArray();
-            return bytes;
-        } catch (Exception e) {
-
-        }
-        return null;
+        // 序列化
+        baos = new ByteArrayOutputStream();
+        oos = new ObjectOutputStream(baos);
+        oos.writeObject(object);
+        byte[] bytes = baos.toByteArray();
+        return bytes;
     }
 
     /**
      * 反序列化
+     *
      * @param bytes
      * @return
      */
-    public static Object unserialize(byte[] bytes) {
+    public static Object unserialize(byte[] bytes) throws Exception {
         ByteArrayInputStream bais = null;
-        try {
-            // 反序列化
-            bais = new ByteArrayInputStream(bytes);
-            ObjectInputStream ois = new ObjectInputStream(bais);
-            return ois.readObject();
-        } catch (Exception e) {
-
-        }
-        return null;
+        // 反序列化
+        bais = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return ois.readObject();
     }
 }
