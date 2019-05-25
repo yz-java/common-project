6f5ce7a95a6309328115e0583dd20927c3ae2edd
yangzhao <yangzhao_java@163.com>
2019-05-25 15:38:46 +0800

    HttpUtil新增获取用户IP方法

--- common-core/src/main/java/com/yz/common/core/utils/HttpUtil.java
+++ common-core/src/main/java/com/yz/common/core/utils/HttpUtil.java
@@ -40,6 +40,31 @@ public class HttpUtil {
 
 	private final static String CHARSET_NAME = "UTF-8";
 
+	/**
+	 * 获取用户真实IP地址
+	 * @param request
+	 * @return
+	 */
+	public static String getIpAddress(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+
 
 	/**
 	 * request对象转字节
