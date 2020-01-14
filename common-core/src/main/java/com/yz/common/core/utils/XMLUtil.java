c6879a515b9b746aeceefd671964bfdba3c09b57
yangzhao <yangzhaojava@gmail.com>
2020-01-14 15:05:37 +0800

    新增xml格式化方法

--- common-core/src/main/java/com/yz/common/core/utils/XMLUtil.java
+++ common-core/src/main/java/com/yz/common/core/utils/XMLUtil.java
@@ -5,9 +5,14 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.*;
 
 /**
@@ -82,5 +87,36 @@ public class XMLUtil {
         }
         sb.append("</xml>");
         return sb.toString();
+    }
+
+    /**
+     * xml格式化
+     * @param inputXML
+     * @return
+     * @throws Exception
+     */
+    public static String formatXML(String inputXML) throws Exception {
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(new StringReader(inputXML));
+        String requestXML = null;
+        XMLWriter writer = null;
+        if (document != null) {
+            try {
+                StringWriter stringWriter = new StringWriter();
+                OutputFormat format = new OutputFormat(" ", true);
+                writer = new XMLWriter(stringWriter, format);
+                writer.write(document);
+                writer.flush();
+                requestXML = stringWriter.getBuffer().toString();
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+        return requestXML;
     }
 }
